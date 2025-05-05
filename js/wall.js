ad3176c325949beef595f903b333267aa9f7ec5d
Daniel <daniel.nicolas.llanes@gmail.com>
2025-05-04 22:17:04 -0300

    post modal funcionality

---
+++ js/wall.js
@@ -0,0 +1,18 @@
+const modal = document.getElementById("post-detail-modal");
+const modalForm = document.getElementById('modal-form');
+
+function openModal() {
+    modal.showModal();
+}
+
+function closeModal() {
+    modal.close();
+    modalForm.reset();
+}
+
+// For close the window with the escape key
+modal.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+        closeModal();
+    }
+});
