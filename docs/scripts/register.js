6178410a92b19513714dc0adbf2438d2fa91c8c9
cotyrodriguez1 <166184231+cotyrodriguez1@users.noreply.github.com>
2025-09-22 11:44:58 -0300

    Add email and password validation with regex and blur events

--- docs/scripts/register.js
+++ docs/scripts/register.js
@@ -1,24 +1,28 @@
 const nameInput = document.getElementById("name");
 const lastnameInput = document.getElementById("lastname");
+const emailInput = document.getElementById("mail");
+const passwordInput = document.getElementById("password");
 
 const nameRegex = /^[A-Za-zÁÉÍÓÚáéíóúñÑ\s]{2,30}$/;
+const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
 
-/*Validation of the name format*/
+/* Validation of the name format */
 function validateName() {
     let nameTrimed = nameInput.value.trim();
     if (!nameRegex.test(nameTrimed)) {
-      nameInput.classList.add("is-invalid");
-      nameError.style.display = "block";
-      return false;
+        nameInput.classList.add("is-invalid");
+        nameError.style.display = "block";
+        return false;
     } else {
-      nameInput.classList.remove("is-invalid");
-      nameError.style.display = "none";
-      return true;
-  }
-};
+        nameInput.classList.remove("is-invalid");
+        nameError.style.display = "none";
+        return true;
+    }
+}
 nameInput.addEventListener("blur", validateName);
 
-/*Validation of the lastname format*/
+/* Validation of the lastname format */
 function validateLastname() {
     let lastnameTrimed = lastnameInput.value.trim();
     if (!nameRegex.test(lastnameTrimed)) {
@@ -31,4 +35,34 @@ function validateLastname() {
         return true;
     }
 }
-lastnameInput.addEventListener("blur", validateLastname)
+lastnameInput.addEventListener("blur", validateLastname);
+
+/* Validation of the email format */
+function validateEmail() {
+    let emailTrimed = emailInput.value.trim();
+    if (!emailRegex.test(emailTrimed)) {
+        emailInput.classList.add("is-invalid");
+        emailError.style.display = "block";
+        return false;
+    } else {
+        emailInput.classList.remove("is-invalid");
+        emailError.style.display = "none";
+        return true;
+    }
+}
+emailInput.addEventListener("blur", validateEmail);
+
+/* Validation of the password format */
+function validatePassword() {
+    let passwordTrimed = passwordInput.value.trim();
+    if (!passwordRegex.test(passwordTrimed)) {
+        passwordInput.classList.add("is-invalid");
+        passwordError.style.display = "block";
+        return false;
+    } else {
+        passwordInput.classList.remove("is-invalid");
+        passwordError.style.display = "none";
+        return true;
+    }
+}
+passwordInput.addEventListener("blur", validatePassword);
