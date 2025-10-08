aa2715a0ac7ca2e1756562c9428c86769ba8cb73
Vero <veronica.ramirez@itscordoba.edu.ar>
2025-10-08 17:15:08 -0300

    Separation of responsibilities

---
+++ docs/scripts/modalPost/modalPost.js
@@ -0,0 +1,47 @@
+class modalPost{
+
+    getModalPost(){
+        const newModalPost = document.createElement("div");
+	    newModalPost.className = "modal-overlay";
+	    newModalPost.innerHTML = ` 
+            <main class="modal-form post-modal-form">
+                <h1 class="visually-hidden" aria-label="Sección crear publicación">Crear una publicación</h1>
+                <div class="modal-header">
+                    <button class="close-button" aria-label="Cerrar ventana">
+                        <i class="fas fa-times"></i>
+                    </button>
+                </div>
+
+                <div class="user-info">
+                    <img src="assets/imgwebp/flavia.webp" alt="Foto de contacto" class="contact-avatar" aria-label="Foto de perfil del usuario">
+                    <h3 aria-label="Nombre de usuario">Flavia Perín</h3>
+                </div>
+
+                <div class="form-container">
+                    <a href="wall.html" class="close-button" aria-label="Cerrar">
+                        <i class="fa-solid fa-xmark"></i>
+                    </a>
+                    <form action="#" method="post" aria-label="Formulario de publicación">
+                        <textarea id="description" aria-label="Contenido de la publicación" placeholder="¿Sobre qué quieres hablar?"></textarea>
+                        <input type="text" id="title" aria-label="Título de la publicación" placeholder="Título">
+                        <input type="text" id="authorship" aria-label="Autoría" placeholder="Autoría">
+                        <textarea id="abstract" aria-label="Resumen" placeholder="Resumen"></textarea>
+
+                        <div class="attachment-section post-attachment-section">
+                            <div class="file-group btn">
+                                <a href="" class="image-button post-image-button" aria-label="Añadir imagen">Adjuntar imagen</a>
+                                <i class="fas fa-image post-fa"></i>
+                            </div>
+                            <div class="file-group btn">
+                                <a href="" class="attach-button post-image-button" aria-label="Adjuntar archivo">Adjuntar documento</a>
+                                <i class="fas fa-paperclip post-fa"></i>
+                            </div>
+                        </div>
+                        <a href="wall.html" class="btn submit-comment"><i class="fas fa-paper-plane" aria-label="Enviar"></i>Enviar</a>
+                    </form>
+                </div>
+            </main>
+            `;
+        return newModalPost;
+    }
+}
---
+++ docs/scripts/modalPost/modalPostPresentation.js
@@ -0,0 +1,45 @@
+//Se encarga de mostrar el modal, agregarlo al DOM, aplicar estilos, agregar listeners, etc.
+function openPostCreationModal(){
+	const modal = new ModalPost();
+	const newModalPost = modal.getModalPost();
+
+	document.body.appendChild(newModalPost);
+	
+	document.querySelector('header').classList.add('blur-active');
+	document.querySelector('main').classList.add('blur-active');
+	document.querySelector('footer').classList.add('blur-active');
+	document.querySelector('.search-bar').classList.add('blur-active');
+	document.querySelector('.menu-nav').classList.add('blur-active');
+
+	const closeButtons = newModalPost.querySelectorAll('.close-button');
+	
+	closeButtons.forEach(btn => {
+		btn.addEventListener('click', event => {
+			event.preventDefault();
+			console.log('Clic en botón de cerrar:', btn);
+			closePostCreationModal(newModalPost);
+			window.location.href = 'wall.html';
+		});
+	});
+
+	const imageButtons = newModalPost.querySelectorAll('.image-button');
+	imageButtons.forEach(btn => {
+		btn.addEventListener('click', event => {
+			event.preventDefault();
+			alert('No implementado por ahora');
+		});
+	});
+
+	const attachButtons = newModalPost.querySelectorAll('.attach-button');
+	attachButtons.forEach(btn => {
+		btn.addEventListener('click', event => {
+			event.preventDefault();
+			alert('No implementado por ahora');
+		});
+	})
+}
+
+function closePostCreationModal(newModalPost) {
+	document.body.removeChild(newModalPost);
+}
+
