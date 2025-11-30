d590cdc8365073038d2fb0819644d043b12f16ce
Daniel <daniel.nicolas.llanes@gmail.com>
2025-11-30 20:30:08 -0300

    visit profile

--- docs/scripts/Post/Post.js
+++ docs/scripts/Post/Post.js
@@ -59,14 +59,16 @@ class Post {
       })
       .join("\n");
 
-    post.innerHTML = `
-            <section class="post-header" data-user-id="${this.userId}">
-                ${ownerAvatarHTML}
-                <div class="user-info">
-                    <p class="username">${this.nameOwner}</p>
-                    <p class="post-date">${date} ${time}</p>
-                </div>
-                <div class="dropdown-container">
+  post.innerHTML = `
+      <section class="post-header" data-user-id="${this.userId}">
+                <a href="profile.html?userId=${this.userId}" class="profile-link" aria-label="Ver perfil de ${this.nameOwner}" style="text-decoration:none;color:inherit;display:inline-flex;align-items:center;gap:.5rem">
+          ${ownerAvatarHTML}
+          <div class="user-info">
+            <p class="username">${this.nameOwner}</p>
+            <p class="post-date">${date} ${time}</p>
+          </div>
+        </a>
+        <div class="dropdown-container">
                     <button class="dropdown-trigger"><i class="fas fa-ellipsis-v"></i></button>
                     <div class="dropdown-menu">
                         ${isOwnPost
