da818d417a5dc14c35cf3c739ae99feb659eef03
Daniel <daniel.nicolas.llanes@gmail.com>
2025-12-07 14:09:23 -0300

    moved modalPost in components

--- docs/scripts/Post/postPresentation.js
+++ docs/scripts/Post/postPresentation.js
@@ -1,3 +1,21 @@
+/*Click for create post*/
+document.addEventListener('DOMContentLoaded', () => {
+    const postInput = document.querySelector('.post-input');
+    const imageButton = document.querySelector('.image-button');
+    const attachButton = document.querySelector('.attach-button');
+
+    const elements = [postInput, imageButton, attachButton];
+
+    elements.forEach(element => {
+        if (element) {
+            element.addEventListener('click', event => {
+            event.preventDefault();
+            openPostCreationModal(); // función importada desde modal.js
+            });
+        }
+    });
+});
+
 // State management for infinite scroll
 let currentPage = 1;
 let isLoading = false;
